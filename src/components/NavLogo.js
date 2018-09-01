da79ec0a9b9de4fbed46edf8aac705e4df9d2601
Nathan Heller <naheller@gmail.com>
2018-09-01 12:24:50 -0700

    burger menu initial

--- src/components/NavLogo.js
+++ src/components/NavLogo.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import '../styles.css'
-import gamepad from '../../static/svg/gamepad-4.svg'
+
+import gamepad from '../../static/svg/gamepad.svg'
+import burgerMenu from '../../static/svg/burger-menu.svg'
 
 const NavLogo = () => (
-    <Link 
-        to="/"
-        key="home"
-        className="nav-logo"
-    >
-        <div className="nav-logo-text">
-            <span className="before-dot-news">ga</span>
-            <img 
-                src={gamepad}
-                className="nav-logo-svg"
-                alt="Gamepad News"
-            />
-            <span className="before-dot-news">epad</span>
-        </div>
-    </Link>
+    <div className="nav-logo-wrapper">
+        <Link 
+            to="/"
+            key="home"
+        >
+            <div className="nav-logo-text">
+                <span className="before-dot-news">ga</span>
+                <img 
+                    src={gamepad}
+                    className="nav-logo-svg"
+                    alt="Gamepad News"
+                />
+                <span>epad</span>
+            </div>
+        </Link>
+        <img 
+            className="burger-menu" 
+            src={burgerMenu} 
+            alt="Nav menu" 
+        />
+    </div>
 )
 
 export default NavLogo;
